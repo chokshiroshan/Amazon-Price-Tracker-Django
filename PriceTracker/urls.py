693806cfc9c7c5bfb7790fc6eac1f27933160be9
brahim024 <ifninos168@gmail.com>
2020-12-10 12:52:13 +0100

    call statis file in url

--- PriceTracker/urls.py
+++ PriceTracker/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.contrib.staticfiles.urls import static
 urlpatterns = [
     path('', include('tracker.urls')),
     path('admin/', admin.site.urls),
 ]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
